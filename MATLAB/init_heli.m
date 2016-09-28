41cba03548bf2e1f0d0e7087f0490f0338c2602b
naked-dan <daniel.nakken2@hotmail.com>
2016-09-28 11:53:53 +0200

    Add labfiles from day 2

--- MATLAB/init_heli.m
+++ MATLAB/init_heli.m
@@ -12,8 +12,8 @@
 
 %%%%%%%%%%% Calibration of the encoder and the hardware for the specific
 %%%%%%%%%%% helicopter
-Joystick_gain_x = 1; % Var 1 /// 0.5
-Joystick_gain_y = 0; % Var -1 /// -1
+Joystick_gain_x = 0; % Var 1 /// 0.5
+Joystick_gain_y = -0.5; % Var -1 /// -1
 
 
 %%%%%%%%%%% Physical constants
@@ -26,21 +26,24 @@ m_p = 0.72; % Motor mass [kg]
 
 
 %%%%%%%%%%% User defined constants
-V_s_s = 7.5; % Vs* voltage Vs when e~, p~, lambda~, and their derivatives are 0
+%V_s_s = 7.5; % Vs* voltage Vs when e~, p~, lambda~, and their derivatives are 0
 J_p = 2*m_p*l_p^2;
 J_e = m_c*l_c^2+2*m_p*l_h^2;
 J_lambda = m_c*l_c^2+2*m_p*(l_h^2+l_p^2);
 
 
 
+
+
 L_2 = m_c*g*l_c-2*m_p*g*l_h;
-K_f = -L_2/(l_h*V_s_s);
 L_1 = l_p*K_f; % Pitch is defined as positive in the counter clockwise direction
-
 L_3 = l_h*K_f;
+V_s_s = -L_2/L_3
+K_f = -L_2/(l_h*V_s_s);
 
 
-K_1 = L_1/J_e;
+K_1 = L_1/J_p;
+K_2 = L_3/J_e;
 
 
 %{
@@ -58,3 +61,13 @@ margin(tran);
 K_pp = 12.5;
 K_pd = 0.7*K_pp;
 K_rp = -2;
+
+A=[0 1 0; 0 0 0; 0 0 0];
+B=[0 0; 0 K_1; K_2 0];
+Q=diag([1/(pi/64)^2 ; 1 ; 1/(pi/64)^2]);
+%Q=diag([9 0.1 25]);
+
+R=[1 0; 0 1];
+K=lqr(A,B,Q,R)
+C=[1 0 0; 0 0 1]
+P = inv(C*inv(B*K-A)*B)
