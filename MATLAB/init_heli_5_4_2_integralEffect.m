fea682ce4ae2943b78ce158d0c96b8a9c405c30c
Sindre Hansen <sindrehansen92@gmail.com>
2016-10-20 18:21:10 +0200

    Add some tuning

--- MATLAB/init_heli_5_4_2_integralEffect.m
+++ MATLAB/init_heli_5_4_2_integralEffect.m
@@ -60,37 +60,22 @@ P = [0 K(1,3); K(2,1) 0];
 % B_e = [0 0; 0  K_1; 0 0; K_2 0; 0 0;  0 0; 0 0; 0 0];
 A_e = [0 1 0 0 0 0; 0 0 0 0 0 0; 0 0 0 1 0 0; 0 0 0 0 0 0; 0 0 0 0 0 1; K_3 0 0 0 0 0];
 B_e = [0 0; 0  K_1; 0 0; K_2 0; 0 0;  0 0];
-C_e = [ 1 0 0 0 0 0 0 0; 0 0 1 0 0 0 0 0; 0 0 0 0 1 0 0 0];
+C_e = [ 1 0 0 0 0 0; 0 0 1 0 0 0; 0 0 0 0 1 0];
 ctrl_poles = eig(A-B*K);
 
 %Largest radius of the controller poles:
 max_rad_ctrl = norm(max(ctrl_poles));
-observer_poles = [];
 
 angleStep = 5;
-r = -max_rad_ctrl*60;
+r = -max_rad_ctrl*6;
 observer_poles = zeros(1, 6);
-x = zeros(1, 6);
-y = zeros(1, 6);
 for i = 1:3
     height = r*sin(pi*angleStep*i/180);
     width = sqrt(r^2 - height^2);
     observer_poles(i) = complex(-width, height);
     observer_poles(i+3) = complex(-width, -height);
-    x(i) = -width;
-    x(i+3) = -width;
-    y(i) = height;
-    y(i+3) = -height;
-    %poles(i) = -max_rad_ctrl*10 - i;
 end
 
-plot(x, y, '*')
-xlim([-10 10])
-ylim([-10 10])
-xL = xlim;
-yL = ylim;
-line([0 0], yL);
-line(xL, [0 0]);
 L = place(A_e', C_e', observer_poles)';
 
 
--- MATLAB/inti_heli_5_4_3.m
+++ MATLAB/inti_heli_5_4_3.m
@@ -49,41 +49,43 @@ K_rp = -2;
 % %Problem 5.3.3
 A = [0 1 0 0 0; 0 0 0 0 0 ; 0 0 0 0 0 ; 1 0 0 0 0; 0 0 1 0 0];
 B=[0 0; 0 K_1; K_2 0; 0 0; 0 0];
-Q=diag([15 0.1 25 1 1]);
-R=[1 0; 0 1];
+Q=diag([.02 3 6 1 1]);
+R=[.03 0; 0 .03];
 K=lqr(A,B,Q,R);
 C=[1 0 0 0 0; 0 0 1 0 0];
 P = [0 K(1,3); K(2,1) 0];
 
 % Problem 5.4.2
-A_e = [ 0 1 0 0 0 0 0 0; 0 0 0 0 0 0 0 0; 0 0 0 1 0 0 0 0; 0 0 0 0 0 0 0 0; 0 0 0 0 0 1 0 0; K_3 0 0 0 0 0 0 0; 1 0 0 0 0 0 0 0; 0 0 0 1 0 0 0 0];
-B_e = [0 0; 0  K_1; 0 0; K_2 0; 0 0;  0 0; 0 0; 0 0];
-C_e = [0 0 1 0 0 0 0 0; 0 0 0 0 1 0 0 0];
+A_e = [ 0 1 0 0 0 0; 0 0 0 0 0 0; 0 0 0 1 0 0; 0 0 0 0 0 0; 0 0 0 0 0 1; K_3 0 0 0 0 0];
+B_e = [0 0; 0  K_1; 0 0; K_2 0; 0 0;  0 0];
+C_e = [0 0 1 0 0 0; 0 0 0 0 1 0];
 ctrl_poles = eig(A-B*K);
+norm(ctrl_poles)
 
 %Largest radius of the controller poles:
 max_rad_ctrl = norm(max(ctrl_poles));
 
-angleStep = 5;
-r = -max_rad_ctrl*60;
-observer_poles = zeros(1, 8);
-x = zeros(1, 8);
-y = zeros(1, 8);
-for i = 1:4
+angleStep = 4;
+r = -max_rad_ctrl*.5;
+observer_poles = zeros(1, 6);
+for i = 1:3
     height = r*sin(pi*angleStep*i/180);
     width = sqrt(r^2 - height^2);
     observer_poles(i) = complex(-width, height);
     observer_poles(i+3) = complex(-width, -height);
-    x(i) = -width;
-    x(i+4) = -width;
-    y(i) = height;
-    y(i+4) = -height;
-    %poles(i) = -max_rad_ctrl*10 - i;
 end
-ctrl_poles
-observer_poles
-C = [B_e A_e*B_e A_e*A_e*B_e A_e^3*B_e]
 L = place(A_e', C_e', observer_poles)';
+
+figure()
+plot(real(observer_poles), imag(observer_poles), '*')
+xlim([real(min(observer_poles)) 1])
+ylim([min(imag(observer_poles))-1 max(imag(observer_poles))+1])
+xL = xlim;
+yL = ylim;
+line([0 0], yL);
+line(xL, [0 0]);
+hold on
+plot(real(ctrl_poles), imag(ctrl_poles), 'x')
 
 
 
