2dec3d459017267990ca10e0e0a400640adab402
Sindre Hansen <sindrehansen92@gmail.com>
2016-10-20 18:43:25 +0200

    Add minor changes

--- MATLAB/heli_plotting.m
+++ MATLAB/heli_plotting.m
@@ -1,12 +1,14 @@
 
-figure()
-plot(x, y, '*')
-%xlim([-10 10])
-%ylim([-10 10])
-xL = xlim;
-yL = ylim;
-line([0 0], yL);
-line(xL, [0 0]);
+% figure()
+% plot(real(observer_poles), imag(observer_poles), '*')
+% xlim([real(min(observer_poles)) 1])
+% ylim([min(imag(observer_poles))-1 max(imag(observer_poles))+1])
+% xL = xlim;
+% yL = ylim;
+% line([0 0], yL);
+% line(xL, [0 0]);
+% hold on
+% plot(real(ctrl_poles), imag(ctrl_poles), 'x')
 
 
 titles  = {'pitch', 'pitch_dot', 'elevation', 'elevation_dot','travel', 'travel_dot'}
@@ -14,10 +16,10 @@ ylabels = {'pitch', 'pitch_dot', 'elevation', 'elevation_dot','travel', 'travel_
 figure()
 for (i = 1:6)
     subplot(3, 2, i)
-    plot(x_states1.time, x_states1.signals.values(:,i))
+    plot(x_states.time, x_states.signals.values(:,i)*(180/pi), 'b')
     hold on
-    plot(xhat_states1.time, xhat_states1.signals.values(:,i))
+    plot(xhat_states.time, xhat_states.signals.values(:,i)*(180/pi), 'r')
     title(titles(i))
     xlabel('Time (sec)')
-    
+    legend('measured','estimated')
 end
